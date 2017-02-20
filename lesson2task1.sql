1c2796ac9d1794336f93db7a5138af5a8c961e2a
Anna Leonenko <mskozlovskaya@gmail.com>
2017-02-20 13:27:16 -0500

    like string

--- lesson2task1.sql
+++ lesson2task1.sql
@@ -1,17 +1,6 @@
 use [Store]
 go
 
-
---select * from [dbo].[OrdersGoods]
-
---select [name] from [dbo].[Goods] where [price] > (select avg([price]) from [dbo].[Goods])
-
---select [name] from [dbo].[Goods] where [price] > (select max([price]) from [dbo].[Goods] group by categoryID having categoryID = 2)
-
---select [name], (select max(categoryID) from [dbo].Goods) [maxcategoryID] from [dbo].[Goods] 
-
-select * from dbo.Goods as p
-	inner join dbo.OrdersGoods as o
-	on p.id = o.goodID
-	--where o.orderID is null 
---	 where p.id = 2
+select top 3  [name], [price] from dbo.Goods 
+	group by [name], [price]
+	having [name] like 'a%'
