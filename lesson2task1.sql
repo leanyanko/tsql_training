419e42bc081bafc79ce949bcd9c3305536dde11d
Anna Leonenko <mskozlovskaya@gmail.com>
2017-02-20 13:35:17 -0500

    date formats

--- lesson2task1.sql
+++ lesson2task1.sql
@@ -4,3 +4,22 @@ go
 select top 3  [name], [price] from dbo.Goods 
 	group by [name], [price]
 	having [name] like 'a%'
+
+declare @date date = getdate()
+select @date
+
+declare @somedate varchar(10) 
+set @somedate = convert (varchar(10), @date, 104)
+select @somedate as '104'
+
+select convert (varchar(20), getdate(), 105) as '105'
+
+select convert (varchar(20), getdate(), 103) as '103'
+
+select convert (varchar(20), getdate(), 102) as '102'
+
+select convert (varchar(20), getdate(), 101) as '101'
+
+select convert (varchar(20), getdate(), 100) as '100'
+
+select convert (varchar(20), getdate(), 106) as '106'
